66c0bcae1f3a56045c365d084841d1f32ae7e084
Trym Kristiansen <trym@trymk.no>
2017-05-04 18:46:58 +0200

    Removed cached files

--- settings.py
+++ settings.py
@@ -7,3 +7,7 @@ AGENT_COLOR = (255, 255, 255)
 FOOD_COLOR = (0, 255, 0)
 POISON_COLOR = (255, 0, 0)
 GROUND_COLOR = (0, 0, 0)
+
+# Environment settings
+TICKS_PER_SECOND = 20
+SECONDS_PER_GENERATION = 20 * 60 # 1 minute
