07e1cf2f171d5f32a353eddb972f4ceca85a7171
Anders Milje <syntox32@gmail.com>
2017-05-04 22:19:22 +0200

    things are starting to shape up

--- environment.py
+++ environment.py
@@ -1,14 +1,45 @@
 
 from agent import Agent
-from settings import WORLD_BOUNDS, NUM_AGENTS, SIMULATION_TICKS
+from settings import WORLD_BOUNDS, NUM_AGENTS, SIMULATION_TICKS, NUM_FOOD, NUM_POISON
 import neat
 import os
 import time
 import network_visualizer
+import random
 
 class Environment:
+
+    #
+    # Each environment should have a config and a Population
+    # the environment should also do the evaluation of the genomes
+    #
+    # each genome is given in a list with an ID and the actual Genome
+    # we will create an agent for each ID/Genome and use that to simulate
+    #
+    # each evaluation will do improve the network for X generations
+    # we then simulate the each network and observe its actions,
+    # each action will change the properties of the Agent class it was given,
+    # in the simulation we observe the action and what score it got from it,
+    # this is called an evaluation
+    #
+    # each new evaluation will be called an episode
+    # in each episode we store the overall score/fitness
+    # the agents will be visualized for Y seconds before doing another
+    # evaluation of the genomes.
+    #
+    # the genome fitness in our case is for an example how much food it has eaten,
+    # the fitness will drop extremely when the agent eats poison
+    #
+    # the fitness in the beginning of the experiment would probably be the ratio
+    # between food and poison
+    #   fitness = food / poison, or fitness = food / (poison / 2)
+    #
+
     def __init__(self):
         self.agents = {}
+        self.num_agents = None
+        self.invalidate_agents = True
+
         self.food = []
         self.poison = []
 
@@ -51,6 +82,7 @@ class Environment:
         #node_names = {-1: 'x', -2: 'dx', -3: 'theta', -4: 'dtheta', 0: 'control'}
         network_visualizer.draw_net(self.config, winner, True)
 
+
         #network_visualizer.draw_net(self.config, winner, view=True,
         #                   filename="winner-feedforward.gv")
         #network_visualizer.draw_net(self.config, winner, view=True,
@@ -68,13 +100,22 @@ class Environment:
         scores = []
         rewards = {}
         steps = 0
+        self.place_foods()
+        self.place_poisons()
+
         while steps < SIMULATION_TICKS:
             steps += 1
 
             for gid, genome, net in networks:
+                if self.invalidate_agents:
+                    self.init_agents(networks)
+                    self.invalidate_agents = False
+
                 if gid not in self.agents:
                     self.agents[gid] = Agent(gid)
+
                 a = self.agents[gid]
+                print(a.pos)
 
                 inputs =  self.get_scaled_inputs(a)  # a.get_scaled_inputs(self)
                 output = net.activate(inputs)
@@ -86,6 +127,8 @@ class Environment:
                 if gid not in self.rewards:
                     self.rewards[gid]= []
                 self.rewards[gid].append(reward)
+
+            self.respawn_items()
 
         for k in self.rewards:
             scores.append(sum(self.rewards[k]))
@@ -100,59 +143,49 @@ class Environment:
         for gid, genome in genomes:
             forward_networks.append((gid, genome,
                     neat.nn.FeedForwardNetwork.create(genome, config)))
-
         self.simulate(forward_networks)
         print("evaluated genome in {0}".format(time.time() - t0))
 
+    def respawn_items(self):
+        food_count = len(self.food)
+        respawn_chance = (NUM_FOOD - food_count) / NUM_FOOD
+        if random.random() < respawn_chance and food_count < NUM_FOOD:
+            self.food.append(self.get_random_pos())
 
-
-
-#
-# Each environment should have a config and a Population
-# the environment should also do the evaluation of the genomes
-#
-# each genome is given in a list with an ID and the actual Genome
-# we will create an agent for each ID/Genome and use that to simulate
-#
-# each evaluation will do improve the network for X generations
-# we then simulate the each network and observe its actions,
-# each action will change the properties of the Agent class it was given,
-# in the simulation we observe the action and what score it got from it,
-# this is called an evaluation
-#
-# each new evaluation will be called an episode
-# in each episode we store the overall score/fitness
-# the agents will be visualized for Y seconds before doing another
-# evaluation of the genomes.
-#
-# the genome fitness in our case is for an example how much food it has eaten,
-# the fitness will drop extremely when the agent eats poison
-#
-# the fitness in the beginning of the experiment would probably be the ratio
-# between food and poison
-#   fitness = food / poison, or fitness = food / (poison / 2)
-#
-
+        poison_count = len(self.poison)
+        respawn_chance = (NUM_POISON - poison_count) / NUM_POISON
+        if random.random() < respawn_chance and poison_count < NUM_POISON:
+            self.poison.append(self.get_random_pos())
 
     def init(self):
-        self.init_agents()
-
-        self.place_agents()
         self.place_foods()
         self.place_poisons()
 
-    def init_agents(self):
-        pass
-        #for _ in range(self.num_agents):
-        #    a = Agent()
-        #    self.agents.append(a)
+    def get_random_pos(self):
+        x_bound = WORLD_BOUNDS[0] / 2
+        rand_x = random.randrange(-x_bound, x_bound)
+        y_bound = WORLD_BOUNDS[1] / 2
+        rand_y = random.randrange(-y_bound, y_bound)
+        return (rand_x, rand_y)
 
-
-    def place_agents(self):
-        pass
+    def init_agents(self, networks):
+        print("Placing agents...")
+        self.num_agents = len(networks)
+        for gid, genome, net in networks:
+            print(gid)
+            if gid not in self.agents:
+                self.agents[gid] = Agent(gid)
+            a = self.agents[gid]
+            a.set_pos(self.get_random_pos())
 
     def place_foods(self):
-        pass
+        self.food = []
+        print("Placing foods...")
+        for _ in range(NUM_FOOD):
+            self.food.append(self.get_random_pos())
 
     def place_poisons(self):
-        pass
+        self.posion = []
+        print("Placing poisons...")
+        for _ in range(NUM_POISON):
+            self.poison.append(self.get_random_pos())
