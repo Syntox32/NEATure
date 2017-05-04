385ee201fced69a0b610d83ce1c2b07cc5b5c243
Anders Milje <syntox32@gmail.com>
2017-05-05 01:46:39 +0200

    create 'Video' dir if it does not already exist

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-import environment
+import environment, os
 
 if __name__ == '__main__':
-    #run()
+
+    local_dir = os.path.dirname(__file__)
+    video_dir = os.path.join(local_dir, 'Video')
+    if not os.path.isdir(video_dir):
+        print("Could not find 'Video' directory, creating one...")
+        os.makedirs(video_dir)
+
     e = environment.Environment()
     e.run()
-locals()
+locals() # why is this line here again?
